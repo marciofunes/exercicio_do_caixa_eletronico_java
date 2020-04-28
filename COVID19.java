397f8dcc876ca164b5f744391e34f87c89973d1b
marciofunes <marciofunes@gmail.com>
2020-04-28 19:20:09 -0300

    Primeiro comiit, código java

---
+++ COVID19.java
@@ -0,0 +1,17 @@
+
+package covid19;
+
+/**
+ *
+ * @author mfunes
+ */
+public class COVID19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
